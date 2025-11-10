6e37f5754b53c237c7b49348359264e225160051
ali arman <ali110arman@gmail.com>
2025-11-10 23:35:09 +0330

    update

--- dist/js/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-	const toggle = document.querySelector('#menu-toggle');
-	const menu = document.querySelector('#mobile-menu');
-	if (toggle && menu) {
-		toggle.addEventListener('click', () => {
-			menu.classList.toggle('hidden');
-		});
-	}
-});
